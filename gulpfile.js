b020089e633e4b78e6ef5ec30a58e2e5c9d9bcf1
Natalia Maksymovych <nataliya.maksymovych@perfectial.com>
2017-01-26 15:20:48 +0200

    autoprefix, image optimizer

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,58 @@
 var gulp = require('gulp'),
-  browserSync = require('browser-sync').create(),
-  notify = require("gulp-notify"),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer')
+    browserSync = require('browser-sync').create(),
+    notify = require("gulp-notify"),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imageop = require('gulp-image-optimization');
 
+var paths = {
+      sassInputFiles: ['./sass/*.scss'],
+      cssOutputFolder: './css/',
+      htmlInputFiles: ['./**/*.html']
+    },
+    opt = {
+      serv: {
+              server: {
+                baseDir: "./"
+              },
+              port: 80
+            },
+      autopref: {
+                  browsers: ['IE >= 9']
+                },
+      imageOp: {
+          optimizationLevel: 5,
+          progressive: true,
+          interlaced: true
+        }
+    }
 // Static server
 gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
+    browserSync.init(opt.serv);
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+gulp.task('sass', function() {
+    return gulp.src(paths.sassInputFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(opt.autopref))
+        .pipe(gulp.dest(paths.cssOutputFolder))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
+gulp.task('images', function(cb) {
+    return gulp.src(['./img/*.*'])
+        .pipe(imageop(opt.autopref))
+        .pipe(gulp.dest('./img/min')).on('end', cb).on('error', cb);
+});
 
 //////////TASKNAME/////////DEPENDENCY TASK FOR THIS TASK////
-gulp.task('runmyproject', ['browser-sync', 'sass'], function(){
-
-  gulp.src("./*").pipe(notify({
-    message: "I'm Initialized My Lord!",
-    onLast: true
-  }));
-  gulp.watch('./sass/*.scss', ['sass']);
-  gulp.watch("./**/*.html").on('change', browserSync.reload);
+gulp.task('runmyproject', ['sass', 'browser-sync'], function() {
+    gulp.src("./*").pipe(notify({
+        message: "I'm Initialized My Lord!",
+        onLast: true
+    }));
+    gulp.watch(paths.sassInputFiles, ['sass']);
+    gulp.watch(paths.htmlInputFiles).on('change', browserSync.reload);
 });
-
